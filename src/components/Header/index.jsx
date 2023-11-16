3cb11c6f7cafd6ffc419f2e778303c2d7ba7dad8
Fabiany  Lopes <fabiany.lopes.060@ufrn.edu.br>
2023-11-16 13:53:18 -0300

    fix: add rel='noopener noreferrer' to external links

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,10 +5,18 @@ export default function Header() {
     <div className="navbar">
       <h1>Fabiany Lopes</h1>
       <div className="links">
-        <a href="https://www.linkedin.com/in/fabianylopes/" target="blank">
+        <a
+          href="https://www.linkedin.com/in/fabianylopes/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Linkedin
         </a>
-        <a href="https://github.com/fabianylopes" target="blank">
+        <a
+          href="https://github.com/fabianylopes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Github
         </a>
       </div>

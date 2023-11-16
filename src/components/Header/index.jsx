0c5b97ebefdb350d6aef5e7182bb29c6e262c950
Fabiany  Lopes <fabiany.lopes.060@ufrn.edu.br>
2023-11-16 12:16:30 -0300

    feat: navbar

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+
+export default function Header() {
+  return (
+    <div className="navbar">
+      <h1>Fabiany Lopes</h1>
+      <div className="links">
+        <a href="https://www.linkedin.com/in/fabianylopes/" target="blank">
+          Linkedin
+        </a>
+        <a href="https://github.com/fabianylopes" target="blank">
+          Github
+        </a>
+      </div>
+    </div>
+  );
+}
